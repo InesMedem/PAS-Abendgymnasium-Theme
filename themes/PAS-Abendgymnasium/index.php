6205987dd355f18bd64f28c650c6808caf4d5525
InesMedem <inesmedem93@gmail.com>
2024-11-07 15:28:44 +1100

    conexion test 3

--- themes/PAS-Abendgymnasium/index.php
+++ themes/PAS-Abendgymnasium/index.php
@@ -3,7 +3,8 @@
 while(have_posts()) {
     the_post(); ?>
 
-    <h2>Hello</h2>
+    <h2> <a href="<?php the_permalink(); ?>" > <?php the_title();?> </a> </h2>
+    <?php the_content();?>
 
     <?php 
 
