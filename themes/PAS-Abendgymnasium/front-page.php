37f521abbda415bfb90a5ab6ebec8e06eb7c65ef
InesMedem <inesmedem93@gmail.com>
2024-11-08 10:11:26 +1100

    Added content to home page and finished widebar widget logic for childpages

--- themes/PAS-Abendgymnasium/front-page.php
+++ themes/PAS-Abendgymnasium/front-page.php
@@ -6,10 +6,16 @@ get_header(); ?>
 <div class="page-banner">
   <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/library-hero.jpg') ?>)"></div>
   <div class="page-banner__content container t-center c-white">
-    <h1 class="headline headline--large">Welcome!</h1>
-    <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
-    <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
-    <a href="#" class="btn btn--large btn--blue">Find Your Major</a>
+    <h1 class="headline headline--large">Abendschule für Berufstätige Charlottenburg-Wilmersdorf</h1>
+    <h3 class="headline headline--small">Das reguläre Abitur und den schulischen Teil der Hochschulzugangsberechtigung kostenlos erwerben</h3>
+    <a href="#" class="btn btn--large btn--blue">Jetzt Anmelden</a>
+  </div>
+</div>
+
+  <div class="container container--narrow page-section">
+  <div class="generic-content">
+
+    <?php the_content(); ?>
   </div>
 </div>
 
