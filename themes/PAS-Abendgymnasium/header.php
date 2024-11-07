810c85b2f79d996166ae0fbc884586871becf5db
InesMedem <inesmedem93@gmail.com>
2024-11-08 07:04:23 +1100

    created blog page and started looping and adding styles to content

--- themes/PAS-Abendgymnasium/header.php
+++ themes/PAS-Abendgymnasium/header.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 
 <head>
+<meta charset="<?php bloginfo("charset"); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
 </head>
+
+<body <?php body_class(); ?>>
 
 <header class="site-header">
       <div class="container">
@@ -34,4 +38,3 @@
       </div>
     </header>
 
-<body>
