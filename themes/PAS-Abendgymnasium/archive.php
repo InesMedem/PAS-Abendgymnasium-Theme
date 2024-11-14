ae2eafdcd166e3922c08b94aa93713784406f07f
InesMedem <inesmedem93@gmail.com>
2024-11-14 14:37:24 +1100

    updates plugins

--- themes/PAS-Abendgymnasium/archive.php
+++ themes/PAS-Abendgymnasium/archive.php
@@ -10,8 +10,8 @@
 <div class="container container--narrow page-section">
   <?php while (have_posts()) : the_post(); ?>
     <div class="post-item">
-      <h2><a class="headline headline--medium headline--post-title" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-      <div class="metabox">
+    <h2 class="headline headline--medium headline--post-title"><?php the_title(); ?></h2>
+    <div class="metabox">
         <p>Posted By <?php the_author_posts_link(); ?> on <?php the_time('n/j/y'); ?> in <?php echo get_the_category_list(', '); ?></p>
       </div>
 
