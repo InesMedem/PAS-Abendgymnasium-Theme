5d96c0deb524797128f66c07f5c1dec839d9ff6f
InesMedem <inesmedem93@gmail.com>
2024-11-27 17:48:53 +1100

    more tweaking headers

--- themes/PAS-Abendgymnasium/front-page.php
+++ themes/PAS-Abendgymnasium/front-page.php
@@ -51,7 +51,7 @@ get_header(); ?>
             <span class="event-summary__day"><?php echo get_the_date('d'); ?></span>
           </a>
           <div class="event-summary__content">
-            <h5 class="event-summary__title headline headline--tiny">
+            <h5 class="event-summary__title headline headline--small">
               <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
             </h5>
             <p>
--- themes/PAS-Abendgymnasium/page-styles.php
+++ themes/PAS-Abendgymnasium/page-styles.php
@@ -4,83 +4,83 @@ get_header(); ?>
 
 <div class="container container--narrow page-section">
 
-<h1>HEADLINE</h1>
-<h1 class="headline headline--large">
-  This is a Large Headline 
-</h1>
+  <div>
+    <h1>HEADLINE</h1>
+    <h1 class="headline headline--large">
+      This is a Large Headline
+    </h1>
 
-<h2 class="headline headline--large-medium">
-  This is a Large-Medium Headline
-</h2>
+    <h2 class="headline headline--large-medium">
+      This is a Large-Medium Headline
+    </h2>
 
-<h3 class="headline headline--medium">
-  This is a Medium Headline
-</h3>
+    <h3 class="headline headline--medium">
+      This is a Medium Headline
+    </h3>
 
-<h4 class="headline headline--small-plus">
-  This is a Small-Plus Headline
-</h4>
+    <h4 class="headline headline--small-plus">
+      This is a Small-Plus Headline
+    </h4>
 
-<h5 class="headline headline--small">
-  This is a Small Headline 
-</h5>
+    <h5 class="headline headline--small">
+      This is a Small Headline
+    </h5>
 
-<h6 class="headline headline--smaller">
-  This is a Smaller Headline
-</h6>
+    <h6 class="headline headline--smaller">
+      This is a Smaller Headline
+    </h6>
 
-<p class="headline headline--tiny">
-  This is Tiny Text (with "Roboto" font)
-</p>
-
-<h1 class="headline headline--post-title">
-  <a href="#">Post Title Link</a>
-</h1>
-
-<h1>Generic content</h1>
-<h1 class="">
-  This is a Large  (Mobile: 5rem)
-</h1>
-
-<h2 class="">
-  This is a Large-Medium 
-</h2>
-
-<h3 class=" ">
-  This is a Medium  (Mobile: 2.4rem)
-</h3>
-
-<h4 class=" ">
-  This is a Small-Plus 
-</h4>
-
-<h5 class=" ">
-  This is a Small  (Mobile: 1.6875rem)
-</h5>
-
-<h6 class=" ">
-  This is a Smaller 
-</h6>
-
-<p class=" ">
-  This is Tiny Text (with "Roboto" font)
-</p>
-
-<h1 class=" ">
-  <a href="#">Post Title Link</a>
-</h1>
+    <p class="headline headline--tiny">
+      This is Tiny Text (with "Roboto" font)
+    </p>
 
 
 
-<div>
 
-  <!-- Orange Button -->
-  <a href="#" class="btn btn--orange">Orange Button</a>  
+    <h1>
+      This is a h1
+    </h1>
 
-  <!-- Blue Button -->
-  <a href="#" class="btn btn--blue">Blue Button</a>
+    <h2>
+      This is a h2
+    </h2>
 
-</div>
+    <h3>
+      This is a h3
+    </h3>
+
+    <h4>
+      This is a h4
+    </h4>
+
+    <h5>
+      This is a h5
+    </h5>
+
+    <h6>
+      This is a h6
+    </h6>
+
+    <p>
+      This is paragraph
+    </p>
+
+
+    <p class="has-x-large-font-size"> Has X-large font size </p>
+    <p class="has-large-font-size"> Has large font size </p>
+    <p class="has-medium-font-size"> Has medium font size </p>
+    <p class="has-small-font-size"> Has small font size </p>
+
+  </div>
+  <div>
+
+    <!-- Orange Button -->
+    <a href="#" class="btn btn--orange">Orange Button</a>
+
+    <!-- Blue Button -->
+    <a href="#" class="btn btn--blue">Blue Button</a>
+
+  </div>
 
 
 
